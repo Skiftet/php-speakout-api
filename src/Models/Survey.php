b5dfd9afea2b02992c5d5a268c9bf34f5c07c15e
Joel E. Svensson <joeles_93@hotmail.com>
2017-04-18 23:55:16 +0200

    Add url method to Survey model

--- src/Models/Survey.php
+++ src/Models/Survey.php
@@ -8,5 +8,8 @@ namespace Skiftet\Speakout\Models;
  */
 class Survey extends BaseModel
 {
-    //
+    public function url(): string
+    {
+        return $this->client()->endpoint().'/surveys/'.urlencode($this['id']);
+    }
 }
